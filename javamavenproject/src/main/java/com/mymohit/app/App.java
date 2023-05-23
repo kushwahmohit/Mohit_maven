2b1a1f53c75c3e67c3908a6e36d0f86dd3d7527a
Mohit Kushwah <mohit_kushwah@outlook.com>
2023-05-23 12:04:25 +0530

    Maven project jenkin

---
+++ javamavenproject/src/main/java/com/mymohit/app/App.java
@@ -0,0 +1,13 @@
+package com.mymohit.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
